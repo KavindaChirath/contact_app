0c0e3d3ab27eee8cd41e5d3e56ce1657f295b4d7
kavinda chirath <ict22028@std.uwu.ac.lk>
2025-11-18 23:56:29 +0530

    import files create list pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:contact_app/ui/contacts_list/contacts_list_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,23 +19,6 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const ContactListPage(title: 'Contacts'),
-    );
-  }
-}
-
-class ContactListPage extends StatelessWidget {
-  const ContactListPage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const Center(child: Text('Welcome to Contacts App!')),
     );
   }
 }

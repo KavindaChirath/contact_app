a7cbcf59f1b2e03be0e95430697d406e2e09d240
kavinda chirath <ict22028@std.uwu.ac.lk>
2025-11-25 23:32:29 +0530

    Styling the form

--- lib/ui/contact/widget/contact_form.dart
+++ lib/ui/contact/widget/contact_form.dart
@@ -17,16 +17,57 @@ class _ContactFormState extends State<ContactForm> {
       key: _formKey,
       child: ListView(
         children: <Widget>[
-          TextFormField(onSaved: (value) => name = value ?? ''),
-          TextFormField(onSaved: (value) => email = value ?? ''),
-          TextFormField(onSaved: (value) => phoneNumber = value ?? ''),
+          SizedBox(height: 10),
+          TextFormField(
+            onSaved: (value) => name = value ?? '',
+            decoration: InputDecoration(
+              labelText: 'Name',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 10),
+          TextFormField(
+            onSaved: (value) => email = value ?? '',
+            decoration: InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 10),
+          TextFormField(
+            onSaved: (value) => phoneNumber = value ?? '',
+            decoration: InputDecoration(
+              labelText: 'Phone Number',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
 
           ElevatedButton(
             onPressed: () {
               _formKey.currentState?.save();
               print('Name: $name, Email: $email, Phone: $phoneNumber');
             },
-            child: Text('Save Contact'),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.person, size: 18),
+                SizedBox(width: 10),
+                Text('SAVE CONTACT', style: TextStyle(color: Colors.white)),
+              ],
+            ),
+            style: ElevatedButton.styleFrom(
+              iconColor: Colors.white,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
           ),
         ],
       ),

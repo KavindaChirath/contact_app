a6c781e19e0ca68a156098aec4382dae3006a94f
kavinda chirath <ict22028@std.uwu.ac.lk>
2025-11-25 21:33:23 +0530

    add key

--- lib/ui/contact/widget/contact_form.dart
+++ lib/ui/contact/widget/contact_form.dart
@@ -5,9 +5,12 @@ class ContactForm extends StatefulWidget {
 }
 
 class _ContactFormState extends State<ContactForm> {
+  // keys allows us to access widget from a different place in code
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: ListView(
         children: <Widget>[TextFormField(), TextFormField(), TextFormField()],
       ),

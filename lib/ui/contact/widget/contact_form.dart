03bf49388fba2beaa776d4b100ca9a00128abc74
kavinda chirath <ict22028@std.uwu.ac.lk>
2025-11-26 14:25:53 +0530

    Validate the name , email, and phone no to correct format

--- lib/ui/contact/widget/contact_form.dart
+++ lib/ui/contact/widget/contact_form.dart
@@ -20,6 +20,8 @@ class _ContactFormState extends State<ContactForm> {
           SizedBox(height: 10),
           TextFormField(
             onSaved: (value) => name = value ?? '',
+            validator: _validateName,
+
             decoration: InputDecoration(
               labelText: 'Name',
               border: OutlineInputBorder(
@@ -31,6 +33,7 @@ class _ContactFormState extends State<ContactForm> {
           SizedBox(height: 10),
           TextFormField(
             onSaved: (value) => email = value ?? '',
+            validator: _validateEmail,
             decoration: InputDecoration(
               labelText: 'Email',
               border: OutlineInputBorder(
@@ -42,6 +45,7 @@ class _ContactFormState extends State<ContactForm> {
           SizedBox(height: 10),
           TextFormField(
             onSaved: (value) => phoneNumber = value ?? '',
+            validator: _validatePhoneNumber,
             decoration: InputDecoration(
               labelText: 'Phone Number',
               border: OutlineInputBorder(
@@ -53,8 +57,11 @@ class _ContactFormState extends State<ContactForm> {
 
           ElevatedButton(
             onPressed: () {
-              _formKey.currentState?.save();
-              print('Name: $name, Email: $email, Phone: $phoneNumber');
+              // Access the Form using the _formKey
+              if (_formKey.currentState?.validate() ?? false) {
+                _formKey.currentState?.save();
+                print('Name: $name, Email: $email, Phone: $phoneNumber');
+              }
             },
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -72,5 +79,37 @@ class _ContactFormState extends State<ContactForm> {
         ],
       ),
     );
+  }
+
+  // use validator: return an error string or null if the value is in the correct format
+  String? _validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Enter a name';
+    }
+    return null;
+  }
+
+  // Validate the Email Address
+  String? _validateEmail(String? value) {
+    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+
+    if (value == null || value.isEmpty) {
+      return 'Enter an email';
+    } else if (!emailRegex.hasMatch(value)) {
+      return 'Enter a valid email';
+    }
+    return null;
+  }
+
+  // Validate the Email Address
+  String? _validatePhoneNumber(String? value) {
+    final phoneRegex = RegExp(r'^\+?[0-9]{7,15}$');
+
+    if (value == null || value.isEmpty) {
+      return 'Enter a phone number';
+    } else if (!phoneRegex.hasMatch(value)) {
+      return 'Enter a valid phone number';
+    }
+    return null;
   }
 }
